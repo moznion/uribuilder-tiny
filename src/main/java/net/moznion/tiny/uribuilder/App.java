561fca829b22f8ee93e379c952d54c26cbc5eddc
moznion <moznion@gmail.com>
2015-04-16 11:47:31 +0900

    Initial commit

---
+++ src/main/java/net/moznion/tiny/uribuilder/App.java
@@ -0,0 +1,12 @@
+package net.moznion.tiny.uribuilder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
---
+++ src/test/java/net/moznion/tiny/uribuilder/AppTest.java
@@ -0,0 +1,14 @@
+package net.moznion;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void shouldAlwaysPass() {
+		assertTrue(true);
+	}
+}
